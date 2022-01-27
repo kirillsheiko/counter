7d01213e2712d0dbc56c4bd21a8f8cb0bd63ca54
kirillsheiko <kirillsheiko@gmail.com>
2022-01-27 19:04:49 +0300

    Add styles.

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,17 @@
 import {useState} from "react";
+import "./Counter.css"
 
 
 export function Counter() {
     const [value, setValue] = useState<number>(0);
     return (
-        <div>
+        <div className={'wrapper'}>
             <div style={{color:value< 5 ? "green" : "red"}}>{value}</div>
-            <button onClick={() => setValue((value) => value < 5 ? value + 1 : 5)}>inc</button>
-            <button onClick={() => setValue(0)}>reset</button>
+            <button className={'inc'}
+                    onClick={() => setValue((value) => value < 5 ? value + 1 : 5)}
+                    disabled={value === 5 ? true : false}>inc</button>
+            <button className={'reset'}
+                    onClick={() => setValue(0)} disabled={value === 0 ? true : false}>reset</button>
         </div>
     )
 }
