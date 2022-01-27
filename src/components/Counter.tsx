959a8151f621553be72b00174ad7b748af65a07a
kirillsheiko <kirillsheiko@gmail.com>
2022-01-27 17:09:27 +0300

    Add styles.

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,7 +6,7 @@ export function Counter() {
     return (
         <div>
             <div style={{color:value< 5 ? "green" : "red"}}>{value}</div>
-            <button onClick={() => setValue((value) => value < 5 ? value + 1 : 5)} >inc</button>
+            <button onClick={() => setValue((value) => value < 5 ? value + 1 : 5)}>inc</button>
             <button onClick={() => setValue(0)}>reset</button>
         </div>
     )
